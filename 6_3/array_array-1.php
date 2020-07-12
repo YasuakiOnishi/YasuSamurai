d3f048212bff494ce3e38b86c393e936f65cd71e
Yasuaki Onishi <yasucrohn11@gmail.com>
2020-07-12 18:17:11 +0900

    6_3少し複雑な配列

---
+++ 6_3/array_array-1.php
@@ -0,0 +1,11 @@
+<pre>
+<?php
+  $students[1][0] = "はるき";
+  $students[1][1] = "かおる";
+  $students[1][2] = "ひでと";
+  $students[2][0] = "ゆきこ";
+  $students[2][1] = "ゆか";
+  $students[2][2] = "まなみ";
+  var_dump($students);
+?>
+</pre>
---
+++ 6_3/array_array-2.php
@@ -0,0 +1,13 @@
+<?php
+//クラス1
+$students = array("はるき", "かおる", "ひでと");
+for ($i = 0; $i < 3; $i++) {
+  echo "class1" . $students[$i] . "<br>";  
+}
+
+//クラス2
+$students = array("ゆきこ", "ゆか", "まなみ");
+for ($i = 0; $i < 3; $i++){
+    echo "class2" . $students[$i] . "<br>";
+}
+?>
